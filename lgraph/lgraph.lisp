90dd3711dd4c3f77f0b77a852ed54cc57362090f
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2026-05-12 10:39:10 -0700

    initial handout files

---
+++ lgraph/lgraph.lisp
@@ -0,0 +1,7 @@
+;; You may define helper functions here
+
+(defun find-sequence (g1 g2 start target k)
+    ;; TODO: Incomplete function
+    ;; The next line should not be in your solution.
+    (list 'incomplete)
+)
---
+++ zip_submission.sh
@@ -0,0 +1,3 @@
+rm ./hw3-submission.zip
+zip hw3-submission.zip -@ < submission_files.txt
+unzip -l hw3-submission.zip
